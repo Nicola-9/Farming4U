6bf9375c897305534ff090e83d1c70b8d4690a56
EugenioSottile <eugenio.sottile.1999@gmail.com>
2020-07-04 23:13:31 +0200

    Creazione GroundsFragment

--- Farming4U/app/src/main/java/com/gruppodieci/farming4u/BottomNavigationMenu.java
+++ Farming4U/app/src/main/java/com/gruppodieci/farming4u/BottomNavigationMenu.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.gruppodieci.farming4u.fragments.GroundStatusFragment;
+import com.gruppodieci.farming4u.fragments.GroundsFragment;
 import com.gruppodieci.farming4u.fragments.SensorInformationFragment;
 import com.gruppodieci.farming4u.fragments.NewNoteFragment;
 import com.gruppodieci.farming4u.fragments.NotesFragment;
@@ -41,7 +42,12 @@ public class BottomNavigationMenu {
 
                         return true;
                     case R.id.grounds:
-                        activeFragment = null;
+
+                        activeFragment = new GroundsFragment();
+                        replaceFragment(activeFragment);
+
+                        MainActivity.getToolbar().setTitle("Terreni");
+
                         return true;
                 }
 
